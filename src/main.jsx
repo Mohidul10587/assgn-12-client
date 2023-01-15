14f9ccea0a4aebf31812c89dcd895951a4074f0c
mohid <mohid10587@gmail.com>
2023-01-15 20:47:12 +0600

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import 'tw-elements';
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
