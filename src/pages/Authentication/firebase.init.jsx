9f7bbfbe26f7d194eb5e8d82315ef6d8d12edeaa
mohid <mohid10587@gmail.com>
2023-03-22 21:47:38 +0600

    add env to git ignore

--- src/pages/Authentication/firebase.init.jsx
+++ src/pages/Authentication/firebase.init.jsx
@@ -1,19 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   // apiKey:process.env.REACT_APP_API_KEY,
-//   apiKey: "AIzaSyDc1BOUJPIh7N7LS3y6q5WdSquEFhy20Hs",
-//   authDomain: "tools-c2b01.firebaseapp.com",
-//   projectId: "tools-c2b01",
-//   storageBucket: "tools-c2b01.appspot.com",
-//   messagingSenderId: "598507322773",
-//   appId: "1:598507322773:web:4ab376a506931b0d264a9c",
-// };
 const firebaseConfig = {
   apiKey:"AIzaSyDc1BOUJPIh7N7LS3y6q5WdSquEFhy20Hs",
   authDomain:process.env.REACT_APP_AUTH_DOMAIN,
